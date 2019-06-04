f3328b5954215fac8c84a86f308f03808c2edb61
saritazavala <42972998+saritazavala@users.noreply.github.com>
2019-06-04 14:41:27 -0600

    Think it is done

--- Funcional/app/src/main/java/com/example/login/MainActivity.kt
+++ Funcional/app/src/main/java/com/example/login/MainActivity.kt
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.RelativeLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,11 +28,22 @@ class MainActivity : AppCompatActivity() {
         rellay2 = findViewById<RelativeLayout>(R.id.rellay2)
 
         handler.postDelayed(runnable, 2500) //Waiting time
+        var username = findViewById<EditText>(R.id.username)
+        var password = findViewById<EditText>(R.id.pass)
+        var login = findViewById<Button>(R.id.loginB)
+
+        login.setOnClickListener{
+            var verificar = if (username.text.toString().equals("hola") && password.text.toString().equals("adios")) {
+                "Logged"
+            } else {
+                "Failed"
+            }
+            Toast.makeText(this,verificar,Toast.LENGTH_SHORT).show()
+        }
 
     }
 
-    var username = findViewById<EditText>(R.id.username)
-    var password = findViewById<EditText>(R.id.pass)
+
 
 
 
